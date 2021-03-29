1aacf0a623035b6e123556327918d9de2a69e22c
d0561910 <d0561910@mail.fcu.edu.tw>
2021-03-29 15:26:52 +0800

    first Commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+// import { Link } from "gatsby";
+
+import Container from "../Container/Container";
+
+const Header = () => {
+  return (
+    <header>
+      <Container type="content">
+        <p>Coronavirus-Map</p>
+        <ul>
+          <li>
+            {/* <Link to="/">Home</Link> */}
+          </li>
+        </ul>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
